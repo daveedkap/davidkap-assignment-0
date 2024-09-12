617ce23149b67b7dc99e371c5c23e3aebbb1955b
David Kaplansky <dkaplanskybrimmer@gmail.com>
2024-09-12 01:17:53 -0400

    add dummy assignment script for assignment 0

---
+++ add_two_numbers.py
@@ -0,0 +1,10 @@
+# add_two_numbers.py
+def add_two_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    # Example numbers to add
+    number1 = 5
+    number2 = 7
+    sum_result = add_two_numbers(number1, number2)
+    print(f"The sum of {number1} and {number2} is: {sum_result}")
